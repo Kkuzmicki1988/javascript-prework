7f653a8ceb0c592dd2064f773f1a300c34b0b0e6
Kamil Kuzmicki <kkuzmicki1988@gmail.com>
2020-06-03 17:56:11 +0200

    Add content for game called A rock, paper scissors. 1/3

--- js/script.js
+++ js/script.js
@@ -1,19 +1,18 @@
-let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-console.log('Gracz wpisał: ' + playerInput);
+console.log('Wylosowana liczba to: ' + randomNumber);
 
-let playerMove = 'nieznany ruch';
+let computerMove = 'nieznany ruch';
 
-if(playerInput == '1'){
-  playerMove = 'kamień';
+if(randomNumber == 1){
+  computerMove = 'kamień';
 }
 
-else if(playerInput == '2'){
-  playerMove = 'papier';
+if else(randomNumber == 1){
+  computerMove = 'papier';
 }
 
-else if(playerInput == '3'){
-  playerMove = 'nożyce';
+if else(randomNumber == 1){
+  computerMove = 'nożyce';
 }
-
-printMessage('Twój ruch to: ' + playerMove);
+printMessage('Mój ruch to: ' + computerMove);
